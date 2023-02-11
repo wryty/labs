fdd67ab7b4dacc51358c7e54691f853a87e87991
wryty <ikotytes@gmail.com>
2023-02-11 20:35:08 +0300

    feat (lw1): work. kinda

---
+++ lw1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace MyApp 
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Пожалуйста, введите значение переменной и нажмите Enter: ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            if ((a % 3 == 0) && (a % 5 == 0)) {
+                Console.WriteLine("fizzbuzz");
+            }
+            else if (a % 3 == 0) {
+                Console.WriteLine("fizz");
+            }
+            else if (a % 5 == 0) {
+                Console.WriteLine("buzz");
+            }
+        }
+    }
+}
