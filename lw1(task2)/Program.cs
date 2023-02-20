f413a0fd215138ec1327e472405fcb264a3698a7
wryty <ikotytes@gmail.com>
2023-02-20 09:43:49 +0300

    feat(lw1,lw1(task2): add new labs

---
+++ lw1(task2)/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            int[] a = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                a[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            int paireqal, pairzero, paireven, pairend5;
+            paireqal = pairzero = paireven = pairend5 = 0;
+            for (int i = 0; i < n; i++)
+            {
+                if (i == 0)
+                {
+                    if (a[i] == a[i + 1]) paireqal++;
+                    if (a[i] % 2 == 0 && a[i + 1] % 2 == 0) paireven++;
+                    if (Convert.ToString(a[i]) [Convert.ToString(a[i]).Length - 1] == '5' && (Convert.ToString(a[i + 1]) [Convert.ToString(a[i + 1]).Length - 1] == '5') ) pairend5++;
+                    continue;
+                }
+                if (i == a.Length - 1)
+                {
+                    //if (a[i] == a[i - 1]) paireqal++;
+                    //if (a[i] == 0 && a[i - 1] == 0) pairzero++;
+                    //if (a[i] % 2 == 0 && a[i - 1] % 2 == 0) paireven++;
+                    //if (Convert.ToString(a[i])[Convert.ToString(a[i]).Length - 1] == '5' && (Convert.ToString(a[i + 1]) [Convert.ToString(a[i - 1]).Length - 1] == '5')) pairend5++;
+                    break;
+                }
+                //if (a[i] == a[i - 1]) paireqal++;
+                if (a[i] == a[i + 1]) paireqal++;
+                if (a[i] == 0 && a[i + 1] == 0) pairzero++;
+                //if (a[i] == 0 && a[i - 1] == 0) pairzero++;
+                if (a[i] % 2 == 0 && a[i + 1] % 2 == 0) paireven++;
+                //if (a[i] % 2 == 0 && a[i - 1] % 2 == 0) paireven++;
+                if (Convert.ToString(a[i])[Convert.ToString(a[i]).Length - 1] == '5' && (Convert.ToString(a[i + 1])[Convert.ToString(a[i + 1]).Length - 1] == '5')) pairend5++;
+                //if (Convert.ToString(a[i])[Convert.ToString(a[i]).Length - 1] == '5' && (Convert.ToString(a[i - 1])[Convert.ToString(a[i - 1]).Length - 1] == '5')) pairend5++;
+            }
+
+            Console.WriteLine("Пар соседей, равных между собой: " + paireqal);
+            Console.WriteLine("Пар соседей, равных нулю: " + pairzero);
+            Console.WriteLine("Пар чётных соседей: " + paireven);
+            Console.WriteLine("Пар соседей, оканчивающихся на 5: " + pairend5);
+        }
+    }
+}
--- lw1/Program.cs
+++ lw1/Program.cs
@@ -6,19 +6,28 @@ namespace MyApp
     {
         static void Main(string[] args)
         {
-            int[,] nums = {  {1,2,3},
-                             {4,5,6},
-                             {7,8,9},
-            };
-
-            for (int i = 0, sum = 0; i < 3; i++) {
-                sum = 0;
-                for (int j = 0; j < 3; j++) {
-                    sum+=nums[j,i];
-                }
-                Console.WriteLine("Сумма " + (i+1) + " столбца: " + sum);
+            int n = Convert.ToInt32(Console.ReadLine());
+            double[] b = new double[n];
+            double p = Convert.ToDouble(Console.ReadLine());
+            double sum = 0;
+            for (int i = 0; i < n; i++) {
+                b[i] = Convert.ToDouble(Console.ReadLine());
             }
+            for (int i = 0; i < n; i++) {
+                if (b[i] > p) {
+                    sum+=b[i];
+                }
+            }
+            Console.WriteLine("Сумма: " + sum);
             
-        }
+
+            
+
+
+
+
+
+            
+        } 
     }
 }
