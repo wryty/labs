2a21236fcfbe3b736f6851dd06679efc6ddbc5e9
wryty <ikotytes@gmail.com>
2023-03-13 09:21:22 +0300

    feat(lw2): new works

---
+++ lw2(task1)/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ConsoleApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputFirst = "hello how are you lmao";
+            string inputSecond = "hello hello how how are you";
+            int wordsCount = 0;
+            string[] wordsFirstString = inputFirst.Split(" ");
+            string[] wordsSecondString = inputSecond.Split(" ");
+
+            for (int i = 0; i < wordsFirstString.Length; i++)
+            {
+                wordsCount = 0;
+                for (int j = 0; j < wordsSecondString.Length; j++)
+                {
+                    if (wordsFirstString[i] == wordsSecondString[j])
+                    {
+                        wordsCount++;
+                    }
+                }
+                Console.WriteLine(wordsFirstString[i] + " - " + wordsCount);
+            }
+        }
+    }
+}
---
+++ lw2(task2)/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Text.RegularExpressions;
+namespace ConsoleApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Regex space = new Regex("  ");
+
+            string inputString = "hello,        how.     are you! longword";
+            while (true)
+            {
+                if (inputString.Contains("  "))
+                {
+                    inputString = space.Replace(inputString, " ");
+                }
+                else { break; }
+            } // можно так
+
+            inputString = Regex.Replace(inputString, "[-.?!)(,:;'[0-9\\]]", ""); // или так
+            string[] wordsString = inputString.Split(" ");
+            int maxLength = -1;
+            string maxLengthWord = "";
+            for (int i = 0; i < wordsString.Length; i++)
+            {
+                if (wordsString[i].Length > maxLength)
+                {
+                    maxLength = wordsString[i].Length;
+                    maxLengthWord = wordsString[i];
+                }
+            }
+
+            Console.WriteLine(maxLengthWord);
+
+        }
+    }
+}
