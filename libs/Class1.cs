ac8ed1647a5c61074734f35404d3eddf1784395a
wryty <ikotytes@gmail.com>
2023-03-25 09:43:45 +0300

    feat(libs): add library

---
+++ libs/Class1.cs
@@ -0,0 +1,37 @@
+﻿namespace libs
+{
+    public class Class1
+    {
+        static public string LTrim(string str)
+        {
+            int spaceIndex = 0;
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (str[i] != ' ')
+                {
+                    spaceIndex = i;
+                    break;
+                }
+            }
+            return str.Substring(spaceIndex);
+        }
+        static public string RTrim(string str)
+        {
+            int spaceIndex = 0;
+            for (int i = str.Length - 1; i >= 0; i--)
+            {
+                if (str[i] != ' ')
+                {
+                    spaceIndex = i;
+                    break;
+                }
+            }
+
+            return str.Substring(0, spaceIndex + 1);
+        }
+        static public string Trim(string str)
+        {
+            return RTrim(LTrim(str));
+        }
+    }
+}
