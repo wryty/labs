3a216b9f52e222432102a3b6369dc2af650c069c
wryty <ikotytes@gmail.com>
2023-03-25 09:47:52 +0300

    feat(libs): add library 2.0

---
+++ probacsharp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ConsoleApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "hello     ";
+            Console.WriteLine(libs.Class1.RTrim(str));
+            string str1 = "     hello";
+            Console.WriteLine(libs.Class1.LTrim(str1));
+            string str2 = "     hello      ";
+            Console.WriteLine(libs.Class1.Trim(str2));
+        }
+
+    }
+}
