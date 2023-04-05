c9dc84bd5ff485a55843bebf29bfc13557dff1b9
wryty <ikotytes@gmail.com>
2023-04-05 19:47:21 +0300

    feat(lw3(task2)): add new task

--- lw3(task2)/Program.cs
+++ lw3(task2)/Program.cs
@@ -4,32 +4,14 @@ namespace ConsoleApplication
 {
     class Program
     {
+        static int PlaceCount(int num) => Math.Abs(num).ToString().Length;
         static void Main(string[] args)
         {
-            Regex space = new Regex("  ");
-
-            string inputString = "hello,        how.     are you! longword";
-            while (inputString.Contains("  "))
-            {
-                inputString = space.Replace(inputString, " ");
-            }
-
-            inputString = Regex.Replace(inputString, "[-.?!)(,:;'[0-9\\]]", "");
-            string[] wordsString = inputString.Split(" ");
-            int maxLength = wordsString[0].Length;
-            string maxLengthWord = wordsString[0];
-            for (int i = 0; i < wordsString.Length; i++)
-            {
-                if (wordsString[i].Length > maxLength)
-                {
-                    maxLength = wordsString[i].Length;
-                    maxLengthWord = wordsString[i];
-                }
-            }
-
-            Console.WriteLine(maxLengthWord);
-
-
+            Console.WriteLine(PlaceCount(2234)); // 4
+            Console.WriteLine(PlaceCount(223)); // 3
+            Console.WriteLine(PlaceCount(-100)); // 3
+            Console.WriteLine(PlaceCount(123321123)); // 9
+            Console.WriteLine(PlaceCount(22341)); // 5
         }
     }
 }
