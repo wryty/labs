453d9ee1a884a747ec4b475e5aaaf28f40e077c2
wryty <ikotytes@gmail.com>
2023-05-13 09:49:22 +0300

    feat(needtomerge): add test code (new)

--- needtomerge/Program.cs
+++ needtomerge/Program.cs
@@ -7,6 +7,7 @@ namespace needtomerge
         static void Main(string[] args) 
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine(123123123);
         }
     }
 }
